66700783ac0aabbfc64594eb942d612835565d2e
Applied Motion Products <158227777+AppliedMotionProducts@users.noreply.github.com>
2025-03-06 11:03:28 -0800

    Update Profile_Velocity_Mode.py

--- Profile_Velocity_Mode.py
+++ Profile_Velocity_Mode.py
@@ -46,7 +46,7 @@ modes_of_operation = {
 def config_func():
     global device
     # target velocity
-    device.sdo_write(0x60FF, 0, bytes(ctypes.c_int32(50000))) #40000 counts = 2rps negative = reverse
+    device.sdo_write(0x60FF, 0, bytes(ctypes.c_int32(50000))) #know your encoder resolution, for some product it is 10000/rev, for some it is 20000/rev
     # profile acceleration
     device.sdo_write(0x6083, 0, bytes(ctypes.c_int32(50000)))
     # profile deceleration
